afb9d861c3aa82b5147355bd1afa3362afbf90bf
TaeGyeong Lee <taegyeong7202@gmail.com>
2019-01-23 09:20:56 +0900

    push

--- alpha_version/difinition.py
+++ alpha_version/difinition.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread('fff.PNG')
+img = cv2.imread('ff.PNG')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 t = np.max(cv2.convertScaleAbs(cv2.Laplacian(gray,3)))
+
 print(t)
