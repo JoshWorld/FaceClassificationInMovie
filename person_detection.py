150eab9330dfea408b3f8a0d419f7914542eded7
TaeGyeong Lee <taegyeong7202@gmail.com>
2019-01-23 13:28:01 +0900

    face and person detection

--- person_detection.py
+++ person_detection.py
@@ -7,35 +7,16 @@ import cv2
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-# Define the video stream
+
+
 cap = cv2.VideoCapture('C:\\Users\\ADMIN\PycharmProjects\\FaceClassificationInMovie\\test_data\\sinsegae2.mp4')  # Change only if you have more than one webcams
 
-# What model to download.
-# Models can bee found here: https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md
 MODEL_NAME = 'ssd_inception_v2_coco_2017_11_17'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
-# Number of classes to detect
 NUM_CLASSES = 1
 
-# Download Model
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-        tar_file.extract(file, os.getcwd())
-
-
-# Load a (frozen) Tensorflow model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -44,30 +25,12 @@ with detection_graph.as_default():
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
 
-
-# Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`,
-# we know that this corresponds to `airplane`.
-# Here we use internal utility functions, but anything that returns a dictionary
-# mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-print(label_map)
 categories = label_map_util.convert_label_map_to_categories(
     label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-print(category_index)
-
-
-# Helper code
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
-
-
 c = 0
-# Detection
 with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
         while True:
